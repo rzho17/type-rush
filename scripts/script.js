7a972a7eca7e6e779847d152876d5248035b537f
Richard <richardzehungho@gmail.com>
2025-07-21 21:51:10 -0700

    add comments for future methods and add the create word method

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,81 @@ class Game {
     this.lives = 3;
     this.hiScore = 0;
     this._elementSelectors();
+    this._test();
   }
 
   _elementSelectors() {
-    this.startBtn = document.querySelector(".start-btn button");
-    this.titleScreen = document.querySelector(".title-screen");
-    this.hiScoreContainer = document.querySelector("#high-score .score");
-    this.currentScoreContainer = document.querySelector("#score .score");
+    this.el = {};
+    this.el.startBtn = document.querySelector(".start-btn");
+    // this.titleScreen = document.querySelector(".title-screen");
+    this.el.gameScreen = document.querySelector("#game-screen");
+    // this.hiScoreContainer = document.querySelector("#high-score .score");
+    // this.currentScoreContainer = document.querySelector("#score .score");
+  }
+
+  _test() {
+    this.el.startBtn.addEventListener("click", () => {
+      console.log("hey");
+    });
   }
   // score
   // high score
+
+  //   get word list from api
+
+  //   get random coord on game board
+
+  // create word component
+  _createWord(word) {
+    const wordBg = document.createElement("div");
+    const bg = document.createElement("div");
+    const overlay = document.createElement("div");
+    const text = document.createElement("span");
+
+    wordBg.classList.add("word");
+    bg.classList.add("bg");
+    text.innerText = word;
+
+    wordBg.append(bg, overlay, text);
+
+    this.el.gameScreen.append(wordBg);
+  }
+
+  // place word on game board
+
+  // typing elements
+
+  // check if word typed === a current word on screen
+
+  // remove word from dom
+
+  // increase score
+
+  // remove life
+  _removeLife() {
+    this.lives--;
+  }
+
+  // check gameover
+
+  // play again or go home
 }
+
+const wordUrl = "https://random-word-api.herokuapp.com/word?length=5&number=10";
+
+const getData = async () => {
+  try {
+    const response = await fetch(wordUrl);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getData();
+const game = new Game();
+
+// game._createWord("heyyyyasdfa");
